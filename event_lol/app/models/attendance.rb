9b6b13e1176c21992d12cebf61939efe96124eda
Mohamed Bouhlel <mohamed.bouhleel@gmail.com>
2019-02-11 16:17:44 +0100

    mmm

--- event_lol/app/models/attendance.rb
+++ event_lol/app/models/attendance.rb
@@ -1,4 +1,6 @@
 class Attendance < ApplicationRecord
+    validates :strip_customer_id, uniqueness: true
+
 belongs_to :user
 belongs_to :event
 end
--- event_lol/app/models/user.rb
+++ event_lol/app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
-    after_create :welcome_send
-
-
-has_many :attendances
-has_many :events, through: :attendances
-has_many :events, foreign_key: 'admin_id'
-
-def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  has_many :attendances
+  has_many :events, through: :attendances
+  has_many :events, foreign_key: 'admin'
 end
